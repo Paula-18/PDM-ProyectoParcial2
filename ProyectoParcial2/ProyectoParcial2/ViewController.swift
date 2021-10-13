19cf0d7ac9114957f9a58360a4f3ff9cdcee1cd6
Paula-18 <53828123+Paula-18@users.noreply.github.com>
2021-10-12 17:19:46 -0700

    Add files via upload

---
+++ ProyectoParcial2/ProyectoParcial2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProyectoParcial2
+//
+//  Created by Alumno on 10/12/21.
+//  Copyright © 2021 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
